d5fa6168508a37cbd91885d2bbc2a02794ab7484
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2024-02-05 01:30:37 +0530

    bankdetails.styles added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import {getBankDetailsPage,submitBankDetails,} from "./routes/bankdetails.js";
 const app = express();
 const port = 3000;
 
@@ -10,26 +11,29 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //   IFSC -> BANK DETAILS
-app.get("/bankdetails", (req, res) => {
-  res.render("bankdetails.ejs");
-});
-app.post("/submit", async (req, res) => {
-  try {
-    const code = req.body.postData;
-    const response = await axios.get(
-      "https://ifsc.razorpay.com/" + code
-    );
-    res.render("bankdetails.ejs", { content: response.data});
-  } catch (error) {
-    console.log("Failed to make request:", error.message);
-    const errorMessage = "Please enter a vaild IFSC code"
-    res.render("bankdetails.ejs", {
-      error: errorMessage,
-    });
-  }
-  
-  //    CURRENCY CONVERTER
-});
+
+app.get("/bankdetails", getBankDetailsPage);
+app.post("/submit", submitBankDetails);
+// app.get("/bankdetails", (req, res) => {
+//   res.render("bankdetails.ejs");
+// });
+// app.post("/submit", async (req, res) => {
+//   try {
+//     const code = req.body.postData;
+//     const response = await axios.get(
+//       "https://ifsc.razorpay.com/" + code
+//     );
+//     res.render("bankdetails.ejs", { content: response.data});
+//   } catch (error) {
+//     console.log("Failed to make request:", error.message);
+//     const errorMessage = "Please enter a vaild IFSC code"
+//     res.render("bankdetails.ejs", {
+//       error: errorMessage,
+//     });
+//   }
+// });
+
+///    CURRENCY CONVERTER
 app.get("/currencyconv",(req,res)=>{
   res.render("currencyconv.ejs");
 });
---
+++ routes/bankdetails.js
@@ -0,0 +1,26 @@
+// bankdetails.js
+import axios from "axios";
+
+const getBankDetailsPage = (req, res) => {
+  res.render("bankdetails.ejs");
+};
+
+const submitBankDetails = async (req, res) => {
+  try {
+    const code = req.body.postData;
+    const response = await axios.get("https://ifsc.razorpay.com/" + code);
+    res.render("bankdetails.ejs", { content: response.data });
+  } catch (error) {
+    console.log("Failed to make request:", error.message);
+    const errorMessage = "Please enter a valid IFSC code";
+    res.render("bankdetails.ejs", {
+      error: errorMessage,
+    });
+  }
+};
+
+export {
+  getBankDetailsPage,
+  submitBankDetails,
+};
+
