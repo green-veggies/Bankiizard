0a0aff8a615b6fab7026289522871d4afc2b02ca
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2024-02-05 23:34:16 +0530

    formatting

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-import {getBankDetailsPage,submitBankDetails,} from "./routes/bankdetails.js";
+import { getBankDetailsPage, submitBankDetails } from "./routes/bankdetails.js";
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//  Homepage
-app.get("/",(req,res)=>{
+//  HOMEPAGE
+
+app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
@@ -19,30 +19,11 @@ app.get("/",(req,res)=>{
 
 app.get("/bankdetails", getBankDetailsPage);
 app.post("/submit", submitBankDetails);
-// app.get("/bankdetails", (req, res) => {
-//   res.render("bankdetails.ejs");
-// });
-// app.post("/submit", async (req, res) => {
-//   try {
-//     const code = req.body.postData;
-//     const response = await axios.get(
-//       "https://ifsc.razorpay.com/" + code
-//     );
-//     res.render("bankdetails.ejs", { content: response.data});
-//   } catch (error) {
-//     console.log("Failed to make request:", error.message);
-//     const errorMessage = "Please enter a vaild IFSC code"
-//     res.render("bankdetails.ejs", {
-//       error: errorMessage,
-//     });
-//   }
-// });
 
 ///    CURRENCY CONVERTER
-app.get("/currencyconv",(req,res)=>{
+app.get("/currencyconv", (req, res) => {
   res.render("currencyconv.ejs");
 });
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
