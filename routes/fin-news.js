2f3e440cdb6dc96da93315fc6cd90b5aa5547215
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2024-03-21 11:18:10 +0530

    all done

--- routes/fin-news.js
+++ routes/fin-news.js
@@ -1,10 +1,9 @@
-
-import axios from "axios";
-// const apiKey = process.env.news_api_key;
 const apiKey = "c2c1d148a8d84732b7629c4990720fed";
+import axios from "axios";
+
 
 const getNews = async(req,res)=>{
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${apiKey}`);
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=`+apiKey);
     res.render("fin-news.ejs",{content:response.data});   
 };
 
